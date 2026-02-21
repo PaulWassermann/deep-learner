55785f594058f8fea3d607680294f675c581087c
Paul Wassermann <paul9499@gmail.com>
2026-02-21 23:36:49 +0100

    feat: add gradient checkers for scalar to scalar and vector to scalar functions

--- src/deep_learner/_gradient_checker.py
+++ src/deep_learner/_gradient_checker.py
@@ -1,13 +1,51 @@
 from collections.abc import Callable
-from typing import Any
+
+import numpy as np
 
 from deep_learner import Tensor
 
-
-# TODO: implement general case function
-def gradient_checking(func: Callable[[Any], Tensor], *args): ...
+EPSILON = 1e-7
 
 
-def gradient_checking_vv(
-    func: Callable[[Tensor], Tensor], parameters_vector: Tensor
-): ...
+def gradient_check_ss(func: Callable[..., Tensor], *inputs: Tensor) -> None:
+    eps = Tensor(EPSILON)
+    r = func(*inputs)
+    r.backward()
+
+    for i in range(len(inputs)):
+        args = list(inputs)
+        args[i] += eps
+        r_pos = func(*args)
+
+        args = list(inputs)
+        args[i] -= eps
+        r_neg = func(*args)
+
+        t = (r_pos - r_neg) / (2 * eps)
+
+        assert np.allclose(t.data, inputs[i].grad.data)
+
+
+def gradient_check_vs(func: Callable[..., Tensor], *inputs: Tensor) -> None:
+    r = func(*inputs)
+    r.backward()
+
+    for i in range(len(inputs)):
+        shape = inputs[i].data.shape
+        partials = np.zeros(shape)
+
+        for index in np.ndindex(shape):
+            eps = np.zeros(shape)
+            eps[index] = EPSILON
+
+            args = list(inputs)
+            args[i] += eps
+            r_pos = func(*args)
+
+            args = list(inputs)
+            args[i] -= eps
+            r_neg = func(*args)
+
+            partials[index] = ((r_pos - r_neg) / (2 * np.linalg.norm(eps))).data
+
+        assert np.allclose(partials, inputs[i].grad.data)
