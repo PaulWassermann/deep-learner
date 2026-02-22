71c11047311fe89ca30bbbda1be164d866c9995b
Paul Wassermann <paul9499@gmail.com>
2026-02-22 10:44:31 +0100

    test: finalize unit testing of scalar to scalar and vector to scalar functions

--- tests/unit/test_backward_implementations.py
+++ tests/unit/test_backward_implementations.py
@@ -1,5 +1,7 @@
 import pytest
 
+from functools import partial
+
 from deep_learner import rand_int, rand_uniform
 from deep_learner._gradient_checker import gradient_check_ss, gradient_check_vs
 from deep_learner.functional.functions import (
@@ -45,29 +47,63 @@ def binary_labels():
     return rand_int(SHAPE, 0, 2, requires_grad=True)
 
 
-def test_add(a, b):
+def test_add_backward(a, b):
     gradient_check_ss(add, a, b)
 
 
-def test_binary_cross_entropy(probs, binary_labels):
+def test_binary_cross_entropy_backward(probs, binary_labels):
     gradient_check_vs(binary_cross_entropy, probs, binary_labels)
 
 
-def test_cross_entropy(probs, binary_labels):
+def test_cross_entropy_backward(probs, binary_labels):
     gradient_check_vs(cross_entropy, probs, binary_labels)
 
 
-def test_divide(a, b):
+def test_divide_backward(a, b):
     gradient_check_ss(divide, a, b)
 
 
-def test_exponential(a):
+def test_exponential_backward(a):
     gradient_check_ss(exponential, a)
 
 
-def test_mean(a):
+def test_mean_backward(a):
     gradient_check_vs(mean, a)
 
 
-def test_mean_squared_error(a, b):
+def test_mean_squared_error_backward(a, b):
     gradient_check_vs(mean_squared_error, a, b)
+
+
+def test_multiply_backward(a, b):
+    gradient_check_ss(multiply, a, b)
+
+
+@pytest.mark.parametrize(("exponent"), [4, 1, -1, 0, -3, 2])
+def test_power_backward(exponent, a):
+    partial_power = partial(power, exponent=exponent)
+    gradient_check_ss(partial_power, a)
+
+
+def test_relu_backward(a):
+    gradient_check_ss(relu, a)
+
+
+def test_sigmoid_backward(a):
+    gradient_check_ss(sigmoid, a)
+
+
+def test_softsign_backward(a):
+    gradient_check_ss(softsign, a)
+
+
+def test_subtract_backward(a, b):
+    gradient_check_ss(subtract, a, b)
+
+
+def test_sum_backward(a):
+    gradient_check_vs(sum, a)
+
+
+def test_tanh_backward(a):
+    gradient_check_ss(tanh, a)
